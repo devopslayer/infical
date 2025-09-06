841da097b5d9dedb266a506962f45a8d620dae33
devopslayer <layerdevops@gmail.com>
2025-09-06 20:59:06 +0530

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,14 @@
-// import { useState } from "react";
 import "./App.css";
-// import Modal from "./components/Modal/Modal";
-// import SwiperCard from "./components/SwiperCard/SwiperCard";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Calendar from "./components/Calendar/Calendar";
 
 function App() {
-  // const [showModal, setShowModal] = useState(false);
-
-  // const toggleModal = () => {
-  //   setShowModal(!showModal);
-  // };
-
   return (
-    <>
-      {/* Only for modal testing purposes, later need to remove both the h1 and button according to the requirements */}
-      {/* <h1>Swiper Card Example - App Component</h1>
-      <button onClick={toggleModal}>Open Modal</button> */}
-
-      <Calendar />
-
-      {/* {showModal && (
-        <Modal showModal={showModal} toggleModal={toggleModal}>
-          <SwiperCard />
-        </Modal>
-      )} */}
-    </>
+    <Router>
+      <Routes>
+        <Route path="/*" element={<Calendar />} />
+      </Routes>
+    </Router>
   );
 }
 
